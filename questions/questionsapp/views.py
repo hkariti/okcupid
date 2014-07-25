8f9a2d8196a1833a76339169d291e51ff06add4e
Hagai Kariti <hkariti@gmail.com>
2014-07-25 19:25:23 +0300

    Add template

--- questions/questionsapp/views.py
+++ questions/questionsapp/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Count
+
+from questionsapp.models import User, Question
 
 # Create your views here.
-from django.http import HttpResponse
 def index(request):
-    return HttpResponse("asd")
+    users_count = User.objects.count()
+    questions_count = Question.objects.count()
+    top10 = Question.objects.annotate(num_users=Count('users')).order_by('-num_users')[:10]
+    context = dict(users_count=users_count, questions_count=questions_count, top10=top10)
+    return render(request, 'questionsapp/index.html', context)
